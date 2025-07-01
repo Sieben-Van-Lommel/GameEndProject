6dc290954481a18b8d5be95922855768a37ff875
Sieben-Van-Lommel <sieben.vanlommel@student.kdg.be>
2025-07-01 16:38:16 +0200

    controllable sprite

--- main.c
+++ main.c
@@ -11,15 +11,33 @@ void main() {
    move_sprite(0, 88, 78);
    SHOW_SPRITES;
 
+   
    while(1){
-      if(currentspriteindex == 0){
-         currentspriteindex = 1;
+      
+
+      switch (joypad()){
+         case J_LEFT:
+            scroll_sprite(0,-10,0);
+            break;
+         
+         case J_RIGHT:
+            scroll_sprite(0,10,0);
+            break;
+
+         case J_UP:
+            scroll_sprite(0,0,-10);
+            break;
+
+         case J_DOWN:
+            scroll_sprite(0,0,10);
+            break;
+      
+      default:
+         break;
       }
-      else{
-         currentspriteindex = 0;
-      }
-      set_sprite_tile(0,currentspriteindex);
-      delay(1000);
-      scroll_sprite(0,10,0);
+
+      delay(100);
+      
    }
+
 }
