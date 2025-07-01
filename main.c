22d7c4c58087cd8e4828f4af0dd67f696797b53b
Sieben-Van-Lommel <sieben.vanlommel@student.kdg.be>
2025-07-01 14:00:32 +0200

    starting new

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "characterController.h"  // Gebruik nu de headerfile
-//#include "background.h"
+
+
 
 void main() {
-    characterController();
-    //background();
+   printf("Hello world");
 }
