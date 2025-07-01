7d2410a150a6d9c2b2217f3ef67df39c0dd3c764
Sieben-Van-Lommel <sieben.vanlommel@student.kdg.be>
2025-07-01 16:13:30 +0200

    moving sprite on screen

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "TestSprite.c"
 
 
 
 void main() {
-   printf("Hello worldd");
+   UINT8 currentspriteindex = 0;
+   set_sprite_data(0,2, TestFace1);
+   set_sprite_tile(0,0);
+   move_sprite(0, 88, 78);
+   SHOW_SPRITES;
+
+   while(1){
+      if(currentspriteindex == 0){
+         currentspriteindex = 1;
+      }
+      else{
+         currentspriteindex = 0;
+      }
+      set_sprite_tile(0,currentspriteindex);
+      delay(1000);
+      scroll_sprite(0,10,0);
+   }
 }
